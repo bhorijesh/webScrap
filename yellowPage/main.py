82be8c9d14b209dcb1c53dd78bfe75ae9f302444
bhorijesh <rijeshbhochhibhoya@gmail.com>
2024-10-03 15:09:01 +0545

    final

--- yellowPage/main.py
+++ yellowPage/main.py
@@ -8,21 +8,26 @@ url = "https://www.yellowpagesnepal.com/agricultural-equipment-and-implements"
 while True:
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
-
-    listing = soup.find_all('h3', class_="font-20 t400")
-    for i in listing:
-        business_name = i.get_text(strip=True)  
-        business_link = i.find('a')['href']  
-        business_link = "https://www.yellowpagesnepal.com/" + business_link
-        Business.append([business_name, business_link])
-       
+    
+    # Finding all the listings on the page
+    lis = soup.find_all('div', id="nav-home")
+    for listing in lis:
+        business_names = listing.find_all('h3', class_="font-20 t400")
+        for i in business_names:
+            business_name = i.get_text(strip=True)
+            business_link = i.find('a')['href']
+            business_link = "https://www.yellowpagesnepal.com/" + business_link
+            Business.append([business_name, business_link])
+    
+    # Find the next page link
     next_page = soup.find('a', string="Next")
     if next_page:
         url = "https://www.yellowpagesnepal.com/" + next_page['href']
     else:
         break
 
-ad = pd.DataFrame(Business , columns=['BusinesName','Businesslink'])
-ad.to_csv("sal.csv")
+# Saving the data into a CSV file
+ad = pd.DataFrame(Business, columns=['BusinessName', 'BusinessLink'])
+ad.to_csv("sal.csv", index=False)
 
-print("Done") 
+print("Done")
--- yellowPage/test.py
+++ yellowPage/test.py
@@ -29,71 +29,78 @@ def extract_business_info(url):
         extract_business_info(url)
 
 extract_business_info(url)
+
+
 ad =pd.read_csv('sal.csv')
+def main(ad):
+    global last_updated, phone, email, Categories, descriptions
 
-
-
-def main():
     
-    for x in ad['Businesslink']:
+    for x in ad['BusinessLink']:
         r = requests.get(x)
         soup = BeautifulSoup(r.content, 'html.parser')
+        
 
-    # Extract "Last Updated" info
-    updated_date = soup.find('div', class_="updated-date float-left")
-    if updated_date:
-        last_updated.append(updated_date.get_text(strip=True))
-    else:
-        last_updated.append(None)
+        # Extract "Last Updated" info
+        updated_date = soup.find('div', class_="updated-date float-left")
+        if updated_date:
+            last_updated.append(updated_date.get_text(strip=True))
+        else:
+            last_updated.append(None)
 
-    # Extract phone and email
-    info = soup.find('div', class_="contact-content")
+        # Extract phone and email
+        info = soup.find('div', class_="contact-content")
 
-    # Extract phone
-    if info is not None:
-        ph = info.find('p')
-        if ph is not None:
-            phone_img = ph.find_all('img')[-1]['src'] if ph.find_all('img') else None
-            if phone_img:
-                phone_number = phone_img.split('=')[1]  # phone number is split by '='
-                phone.append(phone_number)
+        # Extract phone
+        if info is not None:
+            ph = info.find('p')
+            if ph is not None:
+                phone_img = ph.find_all('img')[-1]['src'] if ph.find_all('img') else None
+                if phone_img:
+                    phone_number = phone_img.split('=')[1]  # phone number is split by '='
+                    phone.append(phone_number)
+                else:
+                    phone.append(None)
             else:
                 phone.append(None)
         else:
             phone.append(None)
-    else:
-        phone.append(None)
 
-    # Extract email
-    email_meta = info.find('meta', {'itemprop': "email"}) if info else None
-    if email_meta:
-        email.append(email_meta['content'])  # email is in content attribute
-    else:
-        email.append(None)
+        # Extract email
+        email_meta = info.find('meta', {'itemprop': "email"}) if info else None
+        if email_meta:
+            email.append(email_meta['content'])  # email is in content attribute
+        else:
+            email.append(None)
+            
+        # Extract categories
+        cat = soup.find('div', class_="description mb-5 pt-3")
+        if cat:
+            for cate in cat.find('a'):
+                if cate is not None:
+                    Categories.append(cate.get_text(strip=True))
+        else:
+            Categories.append(None)
         
-    # Extract categories
-    cat = soup.find('div', class_="description mb-5 pt-3")
-    if cat:
-        for cate in cat.find('a'):
-            if cate is not None:
-                Categories.append(cate.get_text(strip=True))
-    else:
-        Categories.append(None)
-    
-        desc_div = soup.find_all('div', id="myTabContent")
+        desc_div = soup.find('div', id="myTabContent")
         if desc_div:
-            desc = desc_div.find('p', {'itemprop': "description"})
+            desc = desc_div.find('div', class_="description mb-5 pt-3")
             if desc:
-                descriptions.append(desc.get_text(strip=True))
-            else:
-                descriptions.append(None)
+                description= desc.find('p', {'itemprop':"description"})
+                if descriptions:
+                    descriptions.append(description.get_text(strip=True))
+                else:
+                    descriptions.append(None)
+            else:   
+                    descriptions.append(None)
         else:
             descriptions.append(None)
-            
-main()
-
         
+                
 
+ 
+if __name__ == "__main__":
+    main(ad)           
 # Ensure all lists are of the same length
 max_length = max(len(address), len(last_updated), len(Categories), len(descriptions), len(phone), len(email))
 
@@ -118,6 +125,8 @@ df = pd.DataFrame({
 
 final_df = pd.concat([ad, df], axis=1)
 
-final_df.to_csv('final.csv',index=False)
+final_df.to_csv('final3.csv',index=False)
+
+
 
 print("Done")
